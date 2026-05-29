7a76976cf0e7585423199517a54ee849787a8213
ivanmesyats <131432370+ivanmesyats@users.noreply.github.com>
2026-05-29 09:37:53 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -43,7 +43,7 @@ export default function BusinessCardWebsite() {
             </div>
             <img src="/images/crystal_logo.png"
                   alt="Crystal United Logo"
-                  className="hidden xl:block w-[220px] h-[220px] object-contain flex-shrink-0 relative z-10"/>
+                  className="hidden lg:block w-[180px] h-[180px] object-contain flex-shrink-0 relative z-10"/>
           </div>
 
           <div className="p-8 md:p-12">
